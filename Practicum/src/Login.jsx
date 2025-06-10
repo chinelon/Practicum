d6c57a92aac83a3c76cf06320087d61cc67a1e1e
chinelon <72803021+chinelon@users.noreply.github.com>
2025-06-10 13:53:03 +0100

    first commit

--- Practicum/src/Login.jsx
+++ Practicum/src/Login.jsx
@@ -14,6 +14,7 @@ function Login() {
     <div className="login">
       <h1>Login Page</h1>
       <p>Please enter your credentials to log in.</p>
+      
       <form onSubmit={handleLogin}>
         <input 
           type="text" 
