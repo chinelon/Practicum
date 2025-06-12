089a3c428a19c55855f8d03513ed469ac3c99e5c
chinelon <72803021+chinelon@users.noreply.github.com>
2025-06-12 16:07:05 +0100

    fixed env issue

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,11 @@ app.use(express.json());
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.connect()
