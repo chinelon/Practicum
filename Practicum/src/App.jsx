c1d64a18303819d5fc054b755f70967d2796ffef
chinelon <72803021+chinelon@users.noreply.github.com>
2025-07-13 19:27:28 +0100

    hopeful anaytics fix

--- Practicum/src/App.jsx
+++ Practicum/src/App.jsx
@@ -11,7 +11,6 @@ import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <Router>
-        <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
