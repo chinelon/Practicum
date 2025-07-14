83ea46582d7f94bf09cb0758f086839ea1b48d84
chinelon <72803021+chinelon@users.noreply.github.com>
2025-07-14 12:56:08 +0100

    more fixes

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -2,16 +2,22 @@ dotenv = require('dotenv');
 const Redis = require('ioredis');
 
 const redisClient = new Redis({
-    username: process.env.REDIS_USERNAME,
+    username: process.env.REDIS_USERNAME || 'default',
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
+        port: Number(process.env.REDIS_PORT),
         tls: true
     }
 });
 
 redisClient.on('connect', () => console.log('Connected to Redis'));
 redisClient.on('error', (err) => console.error('Redis Error:', err));
+console.log('Redis config:', {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  username: process.env.REDIS_USERNAME,
+});
 
 module.exports = redisClient;
